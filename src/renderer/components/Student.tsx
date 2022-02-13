90baa403d43d50c5f3418d46f3bfa0aa2e7b3715
leeminxji <leeminxji@gmail.com>
2022-02-13 18:47:27 +0900

    Fix undefined 2

--- src/renderer/components/Student.tsx
+++ src/renderer/components/Student.tsx
@@ -5,29 +5,17 @@ interface StudentProps {
 }
 
 export default function Student({ student }: StudentProps) {
-  const {
-    intelligence,
-    personality,
-    charm,
-    willingness,
-    luck,
-    sex,
-    knowledge,
-    happiness,
-    credit,
-  } = student;
-
   return (
     <div>
-      <p>intelligence: {intelligence}</p>
-      <p>personality: {personality}</p>
-      <p>charm: {charm}</p>
-      <p>willingness: {willingness}</p>
-      <p>luck: {luck}</p>
-      <p>sex: {sex}</p>
-      <p>knowledge: {knowledge}</p>
-      <p>happiness: {happiness}</p>
-      <p>credit: {credit}</p>
+      <p>intelligence: {student?.intelligence}</p>
+      <p>personality: {student?.personality}</p>
+      <p>charm: {student?.charm}</p>
+      <p>willingness: {student?.willingness}</p>
+      <p>luck: {student?.luck}</p>
+      <p>sex: {student?.sex}</p>
+      <p>knowledge: {student?.knowledge}</p>
+      <p>happiness: {student?.happiness}</p>
+      <p>credit: {student?.credit}</p>
     </div>
   );
 }
