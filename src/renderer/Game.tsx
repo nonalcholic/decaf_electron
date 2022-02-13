7c4e103926ca6e873c02615b2e94d5f0aa7c306d
leeminxji <leeminxji@gmail.com>
2022-02-13 18:43:48 +0900

    Fix undefined

--- src/renderer/Game.tsx
+++ src/renderer/Game.tsx
@@ -64,7 +64,7 @@ export default function Game() {
   return (
     <div>
       <University />
-      {game.isRunning ? (
+      {game?.isRunning ? (
         <button type="button">Running...</button>
       ) : (
         <button type="button" onClick={updateGame}>
